af6caff840a568deb2740ff0dcfacaf560a246b1
Thomasrbm <thomas.robert76@hotmail.com>
2026-03-13 13:36:16 +0100

    First

---
+++ ft_malloc.c
@@ -0,0 +1,20 @@
+#include <sys/mman.h>
+
+void *ft_malloc(size_t size)
+{
+
+
+    mmap(void addr[.length], size_t length, int prot, int flags,
+                  int fd, off_t offset)
+
+    munmap(void addr[.length], size_t length)
+
+
+}
+
+
+
+doit etre multi thread compat pour bonus
+
+manage le debug envi variable
+
---
+++ ft_show_alloc_mem.c
@@ -0,0 +1,21 @@
+
+
+void show_alloc_mem();
+
+
+
+
+he visual will be formatted by increasing addresses such as:
+TINY : 0xA0000
+0xA0020 - 0xA004A : 42 bytes
+0xA006A - 0xA00BE : 84 bytes
+SMALL : 0xAD000
+0xAD020 - 0xADEAD : 3725 bytes
+LARGE : 0xB0000
+0xB0020 - 0xBBEEF : 48847 bytes
+Total : 52698 byte
+
+
+
+bonus : more details, for example,
+a history of allocations, or an hexa dump of the allocated zones.
